3be0cad53c253194385062a9cc3068d01142faa2
Jonathan Huang <jonathanhuang@gatech.edu>
2023-04-08 13:22:13 -0400

    Initial Commit

---
+++ MR Resturant App/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MR Resturant App
+//
+//  Created by Jonathan Huang on 4/8/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ MR Resturant App/MR_Resturant_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MR_Resturant_AppApp.swift
+//  MR Resturant App
+//
+//  Created by Jonathan Huang on 4/8/23.
+//
+
+import SwiftUI
+
+@main
+struct MR_Resturant_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
